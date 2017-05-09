39ffc73ff6abcfb6d976556b605dd118923c1497
Stoecklie <stefan.kienzler@hs-furtwangen.de>
2017-05-09 13:51:18 +0200

    new Testfile

---
+++ src/test/java/kienzler/UtilTest.java
@@ -0,0 +1,35 @@
+package kienzler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UtilTest {
+
+	@Test
+	public void testIstErstesHalbjahr() {
+		boolean test = Util.istErstesHalbjahr(2);
+		
+		if(test){
+			fail("Falsches halbjahr");
+		}
+		
+	}
+	
+	@Test
+	public void testIstErstesHalbjahrFalse() {
+		boolean test = Util.istErstesHalbjahr(2);
+		
+		if(!test){
+			fail("Falsches halbjahr");
+		}
+		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public final void testIstErstesHalbjahrUngueltigeeingabe(){
+		Util.istErstesHalbjahr(13);
+		fail("Exception not throw");
+	}
+}
+
